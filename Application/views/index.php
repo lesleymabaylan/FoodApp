fd2346497add025d1ae27f14a19cff74dd188fe7
Lesley Mabaylan <elmabaylan@gmail.com>
2020-08-31 17:02:15 +0800

    codeigniter connect to db created main controller and index

---
+++ Application/views/index.php
@@ -0,0 +1,4 @@
+
+<div id="container">
+	<h1>Welcome to CodeIgniter!</h1>
+</div>
---
+++ Application/views/template/header.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>FoodApp</title>
+	<link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
